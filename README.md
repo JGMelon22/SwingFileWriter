13f9f3432556fb755cd90cd0eec3cd2d05c89738
JGMelon22 <jgvasconcellos22@gmail.com>
2023-10-10 20:14:12 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# JSwing File Writer
+A simple JSwing project to write inside a JTextPane and create a file within it's content <br />
+<h3>Used Tools</h3>
+<ul>
+    <li>Java 17 <img align="center" alt="JavaIcon" height="4%" width="4%" <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg" /></li>
+    <li>Eclipse IDE <img align="center" alt="EclipseIDE" height="3%" width="3%" <img
+            src="https://github.com/JGMelon22/FilmsAPI-V2/assets/73988556/59e22d83-f5d2-498f-b0f5-49852c1ea312" />
+    </li>
+</ul>
+
+<img src="https://github.com/JGMelon22/DoggoBox/assets/73988556/4797f5c7-79a6-48ae-81bd-bc7170925a71" width="35%" />
+<span>&nbsp;</span>
---
+++ src/MainForm.java
@@ -0,0 +1,116 @@
+import java.awt.EventQueue;
+import java.awt.Image;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.JLayeredPane;
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import java.awt.BorderLayout;
+import java.awt.Cursor;
+
+import javax.swing.JButton;
+import javax.swing.JTextPane;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.awt.event.ActionEvent;
+
+public class MainForm {
+
+	private JFrame frmFileWriter;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainForm window = new MainForm();
+					window.frmFileWriter.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MainForm() {
+
+		try {
+			// Set cross-platform Java L&F (also called "Metal")
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
+		} catch (UnsupportedLookAndFeelException e) {
+			// handle exception
+		} catch (ClassNotFoundException e) {
+			// handle exception
+		} catch (InstantiationException e) {
+			// handle exception
+		} catch (IllegalAccessException e) {
+			// handle exception
+		}
+
+		initialize();
+		frmFileWriter.setResizable(false);
+		frmFileWriter.setLocationRelativeTo(null);
+
+		Image img = Toolkit.getDefaultToolkit().getImage(getClass().getResource("/assets/logo.png"));
+		frmFileWriter.setIconImage(img);
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmFileWriter = new JFrame();
+		frmFileWriter.setTitle("File Writer");
+		frmFileWriter.setBounds(100, 100, 450, 300);
+		frmFileWriter.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JLayeredPane layeredPane = new JLayeredPane();
+		frmFileWriter.getContentPane().add(layeredPane, BorderLayout.CENTER);
+
+		JButton btnWrite = new JButton("Write!");
+
+		btnWrite.setBounds(313, 231, 117, 25);
+		btnWrite.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		layeredPane.add(btnWrite);
+
+		JTextPane textPane = new JTextPane();
+		textPane.setBounds(12, 12, 418, 152);
+		layeredPane.add(textPane);
+
+		btnWrite.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+
+				Thread t1 = new Thread(() -> {
+
+					// Conteúdo texto
+					String texto = textPane.getText();
+
+					String caminhoAtual = Paths.get("").toAbsolutePath().toString();
+					Path path = Paths.get(caminhoAtual + "/Arquivo.txt");
+
+					try {
+						Files.write(path, texto.getBytes());
+					} catch (IOException e) {
+						JOptionPane.showMessageDialog(null, textPane, e.getMessage(), JOptionPane.ERROR_MESSAGE);
+					}
+
+					JOptionPane.showMessageDialog(null, "Arquivo Escrito!", "Mensagem", JOptionPane.PLAIN_MESSAGE);
+				});
+				t1.start();	
+
+			}
+		});
+
+	}
+}
